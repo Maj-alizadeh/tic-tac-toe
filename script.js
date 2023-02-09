51b541c2c7b062f78dbcef005b6958580671dd94
maj-alizadeh <majed.alizadeh@gmail.com>
2023-02-09 20:46:18 +0330

    clean code

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-let gameBoardArray = [];
-let turn = 1;
-let player1Score = 0;
-let player2Score = 0;
 const startButton = document.querySelector(".start-button");
 
+// main game module
 const game = (() => {
+  let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+  let turn = 1;
+  let player1Score = 0;
+  let player2Score = 0;
+  let endGame = false;
+  let counter = 0;
   const display = document.querySelector(".status-display");
+  const player1 = document.getElementById("player1");
+  const player2 = document.getElementById("player2");
+  const playerContainer = document.querySelector(".player-container");
+  const gameBoard = document.querySelector(".game-board");
 
-  const changeTurn = () => {
-    if (turn === 1) turn = 2;
-    else turn = 1;
+  function clearBoard() {
+    for (let i = 0; i < gameBoardArray.length; i += 1) {
+      const cell = document.querySelector(`div[data-cell="${i}`);
+      cell.innerHTML = "";
+    }
+    gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+    endGame = false;
+    counter = 0;
+  }
+
+  const start = () => {
+    if (player1.value === "") {
+      display.innerHTML = "Please enter player names";
+      return;
+    }
+    playerContainer.style.display = "none";
+    display.innerHTML = `${player1.value} ${player1Score} ${player2.value} ${player2Score} `;
+    gameBoard.style.display = "grid";
+    clearBoard();
   };
 
   // checks if a player has won the game
-  function checkWin() {
+  const checkWin = () => {
     const gba = gameBoardArray;
     if (gba[0] === gba[1] && gba[0] === gba[2]) return gba[0];
     if (gba[3] === gba[4] && gba[3] === gba[5]) return gba[3];
@@ -23,15 +46,43 @@ const game = (() => {
     if (gba[2] === gba[5] && gba[2] === gba[8]) return gba[2];
     if (gba[0] === gba[4] && gba[0] === gba[8]) return gba[0];
     if (gba[2] === gba[4] && gba[2] === gba[6]) return gba[2];
+    if (counter >= 9) return "D";
+    return "";
+  };
+
+  function displayWinner() {
+    const winner = checkWin();
+    switch (winner) {
+      case "":
+        break;
+      case "D":
+        display.innerHTML = `It's a Draw, No body wins!`;
+        endGame = true;
+        break;
+      case "X":
+        display.innerHTML = `${player1.value}  wins`;
+        player1Score += 1;
+        endGame = true;
+        break;
+      case "O":
+        display.innerHTML = `${player2.value}  wins`;
+        player2Score += 1;
+        endGame = true;
+        break;
+      default:
+    }
+  }
+
+  function changeTurn() {
+    if (turn === 1) turn = 2;
+    else turn = 1;
   }
 
   // mark the board and store player's marked cell in the array
   const mark = (event) => {
     const index = event.target.dataset.cell;
     const cell = document.querySelector(`div[data-cell="${index}"]`);
-
-    // avoid mark a cell twice
-    if (cell.innerHTML === "") {
+    if (cell.innerHTML === "" && !endGame) {
       if (turn === 1) {
         gameBoardArray[index] = "X";
         cell.innerHTML = "X";
@@ -39,38 +90,10 @@ const game = (() => {
         gameBoardArray[index] = "O";
         cell.innerHTML = "O";
       }
+      counter += 1;
+      displayWinner();
       changeTurn();
     }
-    // checkWin();
-    if (checkWin() === "X") {
-      display.innerHTML = "player 1 wins";
-      player1Score += 1;
-      start();
-    }
-    if (checkWin() === "O") {
-      display.innerHTML = "player 2 wins";
-      player2Score += 1;
-      start();
-    }
-  };
-
-  const start = () => {
-    const playerName = document.querySelector(".player-name");
-    const player1 = document.getElementById("player1");
-    const player2 = document.getElementById("player2");
-    if (player1.value === "") {
-      alert("enter players name");
-      return;
-    }
-    playerName.style.display = "none";
-    display.innerHTML = `${player1.value} ${player1Score} ${player2.value} ${player2Score} `;
-    gameBoard = document.querySelector(".game-board");
-    gameBoard.style.display = "grid";
-    for (let i = 0; i < gameBoardArray.length; i++) {
-      const cell = document.querySelector(`div[data-cell="${i}`);
-      cell.innerHTML = "";
-    }
-    gameBoardArray = [];
   };
 
   const cells = document.querySelectorAll(".cell");
